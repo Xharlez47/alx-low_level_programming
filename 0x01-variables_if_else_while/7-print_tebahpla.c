b03147dafecc00d6831129b8b76cebe87cf506ce
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-11 17:33:51 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * * main - prints alphabet in lowercase followed by a new line
+ * *
+ * * Description: using the main function
+ * * this program prints "Prints any aiphabet in lower case"
+ * * Return: 0
+ */
+int main(void)
+{
+int c;
+for (c = 'z'; c <= 'a'; c++)
+{
+	putchar(c);
+if (c == 'a')
+putchar ('\n');
+}
+return (0);
+}
