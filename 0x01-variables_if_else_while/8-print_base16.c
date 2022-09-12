2cfa66fffb8b7a62d963106a338b42d9064cc1d3
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-12 14:57:35 -0700

    variables_if_else_while cd .. ls :wq ./git variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Description: Using the main function
+ * that prints " prints all the numbers of base 16 in lowercase"
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x >= 0; x++)
+{
+	putchar(x);
+if (x == "%x")
+	putchar(x);
+putchar('\n');
+}
+return (0);
+}
