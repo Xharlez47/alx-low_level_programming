f6a5417e27eac5fb3db48151bbe65ba7659d0080
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 12:49:41 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Using function and nested loops
+ * that prints " Prototype: void print_alphabet(void)"
+ * Return: 0
+ */
+int main(void)
+{
+	void print_alphabet(void);
+	int ab;
+
+	for (ab = 'a'; ab <= 'z'; ab++)
+{
+		putchar(ab);
+}
+_putchar('\n');
+
+return (0);
+}
