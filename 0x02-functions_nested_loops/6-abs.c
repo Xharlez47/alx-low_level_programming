f61ddf0dd365cc33f6de59eccdfdfbb0272a40f1
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 16:39:08 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * Description: Using function and nested loops for a program
+ * that "computes the absolute value of an integer"
+ * Return: int
+ */
+int _abs(int)
+{
+
+if (n > 0)
+{
+	return (n);
+}
+else if (n == 0)
+{
+	return (n);
+}
+else
+{
+	return (-n);
+}
+}
