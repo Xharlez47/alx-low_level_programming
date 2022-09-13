f2b9aefe127bb884d30a360b4338d6672f3818e4
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 04:56:12 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,16 @@
 int main(void)
 {
 int x, y, z;
-for (x = 0; x < 8; x++)
+for (x = '0'; x < '8'; x++)
 {
-	for (y = x + 1; y < 9; y++)
+	for (y = x + '1'; y < '9'; y++)
 {
-	for (z = y + 1; z < 10; z++)
+	for (z = y + 1; z < '10'; z++)
 {
-		putchar(x % 10);
-			putchar(y % 10);
-			putchar(z % 10);
-	if (x != 7)
+		putchar(x % '10');
+			putchar(y % '10');
+			putchar(z % '10');
+	if (x != '7')
 {
 	putchar(',');
 		putchar(' ');
