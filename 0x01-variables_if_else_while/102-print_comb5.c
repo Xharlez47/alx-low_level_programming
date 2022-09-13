5af16f69f8211d73dac905a470e95bf175e61b58
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 05:22:36 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,10 @@ for (x = 0; x < 100; x++)
 {
 	for (y = x + 1; y < 100; y++)
 {
+	putchar((x / 10) + '0');
 		putchar((x % 10) + '0');
+		putchar(' ');
+		putchar((y / 10) + '0');
 			putchar((y % 10) + '0');
 	if (x != 98 || y != 99)
 {
