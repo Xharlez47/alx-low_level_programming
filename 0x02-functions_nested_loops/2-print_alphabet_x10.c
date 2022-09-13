f2cf5fb7277ef89ba8fd4880466a26ad007db75b
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 14:40:25 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: Using function and nested loops for a program
+ * that "print_alphabet in lowercase, followed by a new line x10"
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int ab;
+
+	for (ab = 'a'; ab <= 'z'; ab++)
+{
+	putchar(ab);
+}
+putchar('\n')
+	for (ab = 0; ab < 10; ab++)
+		putchar(ab);
+}
+_putchar('\n');
+}
