d6cecdbc5006bf9fe6f5b014c8f47567c6f511f4
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 04:29:08 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
 int x, y;
 for (x = 0; x <= 7; x++)
 {
-	for (y = 'x' + 1; y <= 8; y++)
+	for (y = x + 1; y <= 8; y++)
 		putchar((x % 10) + '0');
 			putchar((y % 10) + '0');
 	if (x != 8 && y != 9)
