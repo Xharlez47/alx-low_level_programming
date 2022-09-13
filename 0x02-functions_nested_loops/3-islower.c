59e90465c197914146315fe61cde9c2078968800
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 15:29:54 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description: Using function and nested loops for a program
+ * that "Returns 1 if c is lowercase Returns 0 otherwise"
+ * Return: 1 'c' (if true) or 0 (if false)
+ */
+int _islower(int c)
+{
+	int wa = 0;
+	int c = 1;
+	int ya;
+	int za;
+
+	for (ya = 'A'; ya <= 'Z'; ya++)
+{
+		_putchar(wa);
+	for (za = 'a'; za <= 'z'; za++)
+		_putchar(c);
+}
+return (1);
+}
