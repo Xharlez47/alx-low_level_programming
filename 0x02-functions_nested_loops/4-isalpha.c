4f90fdd4a65e72a5f0bf42f17a35db45fc62fcd7
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 15:43:06 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * Description: Using function and nested loops for a program
+ * that "Returns 1 if c is alphabetic Returns 0 otherwise"
+ * @c: the integer value it receives
+ * Return: 1  (if true) or 0 (if false)
+ */
+int _isalpha(int c);
+{
+	int za;
+
+	for (za = '0'; za <= '9'; za++)
+{
+	if (c != za)
+{
+	return (1);
+}
+}
+return (0);
+}
