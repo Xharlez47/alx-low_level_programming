d791dcb3684e25f1ab821c2c56fb8bfd8ad5434a
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-14 12:37:31 -0700

    nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,9 +15,9 @@ c = 2;
 printf("%lu", b);
 for (a = 1; a < 91; a++)
 {
-	printf(", %lu", c);
-	c = c + b;
-	b = c - b;
+printf(", %lu", c);
+c = c + b;
+b = c - b;
 }
 d = b / 1000000000;
 e = b % 1000000000;
@@ -25,12 +25,12 @@ f = c / 1000000000;
 g = c % 1000000000;
 for (a = 92; a < 99; ++a)
 {
-	printf(", %lu", f + (k2 / 1000000000));
-	printf("%lu", k2 % 1000000000);
-	f = f + d;
-	d = f - d;
-	g = g + e;
-	e = g - e;
+printf(", %lu", f + (k2 / 1000000000));
+printf("%lu", k2 % 1000000000);
+f = f + d;
+d = f - d;
+g = g + e;
+e = g - e;
 }
 printf("\n");
 return (0);
