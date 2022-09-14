5a669797230236ad2f8a74f52b3e293a8158f31d
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 18:49:34 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_times_table - Entry point
+ *
+ * Description: Using function and nested loops for a program
+ * that "prints the n times table, starting with 0"
+ * @n: is an integer
+ * Return: int
+ */
+void print_times_table(int n)
+{
+
