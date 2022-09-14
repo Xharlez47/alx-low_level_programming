0240d0228211564859fd545a48dd79e49e29c184
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 18:20:27 -0700

    nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,13 +17,13 @@ while (x < 24)
 y = 0;
 while (y < 60)
 {
-		_putchar((x / 10) + '0');
-		_putchar((x % 10) + '0');
-		_putchar(':');
-		_putchar((y / 10) + '0');
-		_putchar((y % 10) + '0');
-		_putchar('\n');
-		y++;
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+_putchar(':');
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+_putchar('\n');
+y++;
 }
 x++;
 }
