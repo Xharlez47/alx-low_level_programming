3a6c883ac88b7f6d843f7b1add55e7bd295fa8e8
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-14 15:01:31 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,4 +10,24 @@
  */
 void print_times_table(int n)
 {
+	int b;
+	int c = b * n;
 
+	for (n = 0; n <= 15; n++)
+{
+	for (b = 0; b <= 15; b++)
+{
+	if (b <= n)
+{
+	_putchar(c);
+	if (c != 225)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+}
