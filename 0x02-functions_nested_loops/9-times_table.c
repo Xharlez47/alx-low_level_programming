000f8e5956a95330683faa7bbb11a9291a94aa7d
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 17:43:53 -0700

    nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,21 +1,42 @@
 #include "main.h"
 
 /**
- * print_last_digit - Entry point
+ * times_table - Entry point
  *
  * Description: Using function and nested loops for a program
- * that "the last digit of a number"
- * @x: is an integer
- * Return: x
+ * that "the 9 times table, starting with 0"
+ * Return: void
  */
-int print_last_digit(int x)
+void times_table(void)
 {
-int z = x % 10;
+int a, b, c, d, e;
 
-if (x < 0)
+for (a = 0; a <= 9; a++)
 {
-	z = z * -1;
+for (c = 0; c <= 9; c++)
+{
+	d = a * c;
+	b = d / 10;
+	e = d % 10;
+if (c == 0)
+{
+	_putchar('0');
 }
-_putchar(z + '0');
-return (z);
+else if (d < 10)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(e + '0');
+}
+else
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(b + '0');
+	_putchar(e + '0');
+}
+}
+_putchar('\n');
+}
 }
