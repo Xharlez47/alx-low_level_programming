9a7b75bca6b23cdd8bc17ebba21e9cf7f7d2a28f
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 17:51:40 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: Using function and nested loops for a program
+ * that "adds two integers and returns the result"
+ * @a: is an integer
+ * @b: is an integer
+ * Return: sum
+ */
+int add(int a, int b);
+{
+	int sum;
+
+	if (sum = a + b)
+{
+		return (sum);
+}
+}
