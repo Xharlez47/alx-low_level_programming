81ac74e6b6fe0f133222a38f17998269ab8cdd30
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-13 18:28:55 -0700

    nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,8 +10,26 @@
  */
 void print_to_98(int n)
 {
-	int sum;
+int x, y;
 
-	sum = a + b;
-	return (sum);
+if (n <= 98)
+{
+for (x = n; x <= 98; x++)
+{
+if (x != 98)
+_putchar(x);
+else if (x == 98)
+_putchar('\n');
+}
+}
+else if (n >= 98)
+{
+for (y = n; y >= 98; y--)
+{
+if (y != 98)
+_putchar(y);
+else if (y == 98)
+_putchar('\n');
+}
+}
 }
