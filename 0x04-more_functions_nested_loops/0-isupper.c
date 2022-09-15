e25c56811d2c35d72b351d36308d8bae99510e5b
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-15 16:11:41 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,18 +5,16 @@
  * _isupper - checks if an input is a uppercase letter
  *
  * @c: letter
- * Retun: 1
+ * Return: 1
  */
 int _isupper(int c)
 {
-	c = 'A';
-
-if (isupper(c))
-{
-	printf("%c: %d\n, c");
-}
-else
-{
-	printf("%c: %d\n, c");
-}
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
