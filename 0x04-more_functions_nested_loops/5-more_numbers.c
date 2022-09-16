cc13026c57cd518375d89db8379ce4b2e9976866
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-16 12:03:09 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,12 +15,8 @@ for (n = 0; n < 10; n++)
 {
 	for (c = 0; c <= 14; c++)
 {
-	if (c > 9)
-{
-	_putchar((c / 10) + '0');
+	_putchar(c + '48');
 }
-_putchar((c % 10) + '0');
 }
-_putchar(10);
-}
+_putchar('\n');
 }
