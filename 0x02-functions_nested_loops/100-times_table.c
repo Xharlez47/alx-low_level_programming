bac4fedb6db046bb96d7c37ac4225b92eaaa5ca9
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-20 19:43:52 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,44 +7,38 @@
  */
 void print_times_table(int n)
 {
-	int b, c, d;
+	int a, b, c;
 
-if (n >= 0 && n <= 15)
-{
-for (b = 0; b <= n; b++)
-{
-for (c = 0; c <= n; c++)
-{
-d = b * c;
-if (b == 0)
-{
-_putchar(d + '0');
-}
-else if (d < 10 && b != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(d + '0');
-}
-else if (d >= 10 && d < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((d / 10) + '0');
-_putchar((d % 10) + '0');
-}
-else if (d >= 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((d / 100) + '0');
-_putchar(((d / 10) % 10) + '0');
-_putchar((d % 10) + '0');
-}
-}
-_putchar('\n');
-}
-}
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar(48);
+			for (b = 1; b <= n; b++)
+			{
+				c = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (c <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(c + 48);
+				}
+				else if (c <= 99)
+				{
+					_putchar(32);
+					_putchar((c / 10) + 48);
+					_putchar((c / 10) + 48);
+				}
+				else
+				{
+					_putchar(((c / 100) % 10) + 48);
+					_putchar(((c / 10) % 10) + 48);
+					_putchar((c % 10) + 48);
+				}
+			}
+				_putchar('\n');
+		}
+	}
 }
