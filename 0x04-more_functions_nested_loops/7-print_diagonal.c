9b2ea27028a1d0c0c7d049a08a4b7fda4fe98def
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-21 01:11:17 -0700

    -more_functions_nested_loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,21 +10,20 @@ void print_diagonal(int n)
 {
 	int a, b;
 
-	if (n > 0)
-{
-	for (a = 0; a < n; a++)
-{
-	for (b = 0; b < n; b++)
-{
-	if (a == b)
-{
-	_putchar(92);
+	if (n <= 0)
+	{
 	_putchar('\n');
-}
+	}
 	else
-		_putchar(' ');
-}
-}
-_putchar('\n');
-}
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < n; b++)
+			{
+			_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+			}
+		}
 }
