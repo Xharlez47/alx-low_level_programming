68df618f958f3e932494cf81b184c5fa280781ad
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-26 17:27:45 -0700

    more arrays and pointers

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  print_chessboard - a function that prints a chessboard.
+ *  @a: the chessboard to be printed.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int black;
+	int white;
+
+	for (black = 0; black < 8; black++)
+	{
+		for (white = 0; white < 8; white++)
+		{
+			_putchar(a[black][white]);
+		}
+	}
+}
