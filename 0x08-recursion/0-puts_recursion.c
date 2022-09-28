3ff93fab372506f7aca3b84fb10d783dc5716ee9
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-28 09:07:45 -0700

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,5 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		_putchar(*(s + i));
-	}
-	_putchar('\n');
+	puts(s);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char);
 void _puts_recursion(char *s);
 
