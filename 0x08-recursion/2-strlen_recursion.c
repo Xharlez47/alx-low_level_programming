95945eff5a7d929d98adf19b2cca1450e59c1b9a
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-28 12:26:35 -0700

    Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,8 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
-	if (*s)
-	{
-	_strlen_recursion(s + 1);
-	_putchar(i);
-	}
-	return (*(s + i));
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
