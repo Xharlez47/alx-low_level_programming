9ded5df5b2800511918a40deda089c6819b22f9a
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-30 15:29:37 -0700

    argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,29 @@
 #include "main.h"
 
 /**
+ * arrayv - Prints the number of string
+ * @i: integer index
+ * Return: void
+ */
+
+void arrayv(int i)
+{
+	if (i / 10)
+	{
+		arrayv(i / 10);
+		_putchar(i + 48);
+	}
+}
+/**
  * main - a program that prints the number of arguments passed into it
  * @argc: number of the arguments on the command line
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-	
-	for (i = 0; *(argv + i); i++)
-	{
-		return (argc);
-	}
+	arrayv(argc - 1);
+	_putchar(argc);
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - a program that prints all arguments it receives
+ * @argv: name of the command file
+ * @argc: number of the arguments on the command line
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i;
+
+	for  (i = 0; i < argc; i++)
+	{
+	puts(argv[i]);
+	}
+	return (0);
+}
