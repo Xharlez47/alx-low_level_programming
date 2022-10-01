04f082545938aac5826a3c86c7cf5765a30d3f47
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-30 19:13:39 -0700

    args -argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,4 +9,20 @@
 
 int main(int argc, char *argv[])
 {
+	int i, res;
 
+	res = 0;
+	for (i = 0; i < argc; i++)
+	{
+		if (i > 0)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			return (printf("Error\n"), 1);
+		}
+		res += atoi(argv[i]);
+	}
+	}
+	printf("%d\n", res);
+	return (0);
+}
