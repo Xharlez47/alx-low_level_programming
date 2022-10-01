909cad354fe0746ab6f8b9e018d45d6922315f5d
Xharlez47 <Charleskelvin47@gmail.com>
2022-09-30 19:21:19 -0700

    args -argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,24 +4,24 @@
  * main - a  program that adds positive numbers.
  * @argv: name of the command file
  * @argc: number of the arguments on the command line
- * Return: 0 if no umber i input , 1 if the numbers are not integers
+ * Return: 0 if number i input , 1 if the numbers are not integers
  */
 
 int main(int argc, char *argv[])
 {
-	int i, res;
+	int i;
+	int res = 0;
 
-	res = 0;
 	for (i = 0; i < argc; i++)
 	{
 		if (i > 0)
-	{
-		if (atoi(argv[i]) == 0)
 		{
+			if (atoi(argv[i]) == 0)
+			{
 			return (printf("Error\n"), 1);
-		}
+			}
 		res += atoi(argv[i]);
-	}
+		}
 	}
 	printf("%d\n", res);
 	return (0);
