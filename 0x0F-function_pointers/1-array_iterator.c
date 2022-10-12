e328b322b5c957a25f1cea3e0e76aac49aef394e
Xharlez47 <Charleskelvin47@gmail.com>
2022-10-12 13:50:52 -0700

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator -  executes a function given as a parameter on 
+ * array_iterator -  executes a function given as a parameter on
  * each element of an array
  * @array: is the pointer
  * @size:  is the size of the array
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,16 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, *j;
+	int i;
 
 	if (array == NULL || cmp == NULL)
-		return;
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i <= size; i++)
-		(*cmp)(array[i]) = j[i];
-	return (0);
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
 }
