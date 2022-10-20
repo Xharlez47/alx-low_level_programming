2cb96e97ab24674bf76d6b38d0785dc91793b263
Xharlez47 <charleskelvin47@gmail.com>
2022-10-20 11:08:02 -0700

    lists link

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new)
 		return (NULL);
 	new->str = strdup(str);
-	new->index = index
+	new->index = index;
 	new->next = (*head);
 	(*head) = new;
 	return (*head);
