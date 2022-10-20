d0e026b753ead521ae82827175730088bd93b058
Xharlez47 <charleskelvin47@gmail.com>
2022-10-20 12:14:32 -0700

    lists link

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) justprint(void);
+
+/**
+ * justprint - prints a string before the
+ */
+
+void justprint(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
