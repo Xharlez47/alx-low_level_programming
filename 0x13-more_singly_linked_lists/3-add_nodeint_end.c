5fceec1dd4d05575fe1a5e5be5b2f7027f98fdfd
Xharlez47 <charleskelvin47@gmail.com>
2022-10-25 04:56:04 -0700

    more linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,7 +7,7 @@
  * Return: the address of the new element, or NULL if it fails
  */
 
-stint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *temp, *temp2;
 
