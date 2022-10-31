a8c461bd2f55e54526cfc59b29214f96151af6de
Xharlez47 <charleskelvin47@gmail.com>
2022-10-31 10:53:12 -0700

    file manipulation

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * read_textfile - hat reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: string
+ * @letters: characters
+ * Return: 0 if filename is NULL or if write fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	unsigned int i;
+
+	if (!filename)
+		return (0);
+	if (
+}
