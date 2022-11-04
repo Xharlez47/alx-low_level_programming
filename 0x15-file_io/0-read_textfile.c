23223a11a83785ae6e984b10697b31d0520372ab
Xharlez47 <charleskelvin47@gmail.com>
2022-11-04 11:44:07 -0700

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,8 +16,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		exit(1);
+	if (fd < 0)
+		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
